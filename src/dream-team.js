3759bf0ae4fbf84bc7edef6779e97dc38d12c333
Mad Pillow <dmtr.schv@gmail.com>
2021-02-08 21:08:52 +0200

    feat: dream-team solved

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  let teamName = "";
+  let membersProcessed = [];
+
+  if (!Array.isArray(members)) {
+    return false;
+  }
+
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === "string") {
+      membersProcessed[i] = members[i].replace(/\s+/g, "");
+      membersProcessed[i] = membersProcessed[i].toUpperCase();
+    }
+  }
+
+  membersProcessed = membersProcessed.sort();
+
+  for (let i = 0; i < membersProcessed.length; i++) {
+    if (typeof membersProcessed[i] == "string") {
+      teamName = teamName + membersProcessed[i][0];
+    }
+  }
+
+  return teamName;
 };
