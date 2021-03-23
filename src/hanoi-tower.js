ed7ba27f1827ad95ba6e1f05066defda7b7cbddd
Svibex <77160861+Svibex@users.noreply.github.com>
2021-03-23 13:04:31 +0300

    Update hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,6 +1,7 @@
-const CustomError = require("../extensions/custom-error");
-
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  let res = Math.pow(2, disksNumber) - 1;
-  return {turns: res, seconds:  Math.floor(res / (turnsSpeed / 3600))}
-};
+    const turns = Math.pow(2, disksNumber) - 1;
+    return {
+        turns,
+        seconds: Math.floor(turns/(turnsSpeed/3600))
+    };
+}
