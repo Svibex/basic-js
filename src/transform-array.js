9eee741cc9ed8e8dabade9dca48dbb28ec612dba
Svibex <future.science@yandex.ru>
2022-04-29 12:01:33 +0300

    update

--- src/transform-array.js
+++ src/transform-array.js
@@ -14,49 +14,45 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 function transform(arr) {
-  throw new NotImplementedError('Not implemented');
   if (!Array.isArray(arr)) throw Error('\'arr\' parameter must be an instance of the Array!');
   if (!arr.length) return [];
   const obj = arr.reduce((acc, el, i) => {
     if (el === '--double-next') {
       if (i+1 < arr.length) {
-        acc[i + 1] ? acc[i + 1] = acc[i + 1] + 1 : acc[i + 1] = 1;
+        acc[i + 1] = 2;
       }
       acc[i] = -1;
       return acc;
     }
     if (el === '--double-prev') {
       if (i-1 > 0) {
-        acc[i-1] ? acc[i-1] = acc[i-1] + 1 : acc[i-1] = 2;
+        acc[i-1] === -1 ? acc[i-1] = -1 : acc[i-1] = acc[i-1] + 1;
       }
       acc[i] = -1;
       return acc;
     }
     if (el === '--discard-next') {
       if (i+1 < arr.length) {
-        acc[i + 1] ? acc[i + 1] = acc[i + 1] - 1 : acc[i + 1] = -1;
+        acc[i + 1] = -1;
       }
       acc[i] = -1;
       return acc;
     }
     if (el === '--discard-prev') {
       if (i-1 > 0) {
-        acc[i - 1] ? acc[i - 1] = acc[i - 1] - 1 : acc[i - 1] = -1;
+        acc[i - 1] === -1 ? acc[i - 1] = -1 : acc[i - 1] = acc[i - 1] - 1;
       }
       acc[i] = -1;
       return acc;
     }
-    acc[i] ? acc[i] = acc[i] + 1 : acc[i] = 1;
+    acc[i] ? acc[i] = acc[i] : acc[i] = 1;
     return acc;
   }, {});
   const result = [];
   for (let key in obj) {
-    if (obj[key] === 1) {
+    while(obj[key] > 0) {
       result.push(arr[key]);
-    }
-    if (obj[key] === 2) {
-      result.push(arr[key]);
-      result.push(arr[key]);
+      obj[key]--;
     }
   }
   return result;
